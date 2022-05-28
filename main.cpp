0f8f72d9fd696901bf352bcfa8a4aad9912e639d
mvjkel1 <mvjkel9@gmail.com>
2022-05-28 14:43:55 +0200

    Finished.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+#include <fstream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverse(string &inputString) {
+    stack<int> myStack;
+
+    for (auto var: inputString) {
+        myStack.push(var);
+    }
+
+    for (int i = 0; i < inputString.length(); i++) {
+        inputString[i] = myStack.top();
+        myStack.pop();
+    }
+}
 
 int main() {
-    std::cout << "Hello, world!\n";
+    fstream inputFile;
+    string output = "";
+    string text = "";
+    inputFile.open("C:\\Users\\mvjkel1\\Desktop\\studia\\Mazur\\Mazur_lab\\znaki1.txt");
+    if (inputFile.is_open()) {
+        while (getline(inputFile, output)) {
+            text += output + '\n';
+        }
+    }
+    inputFile.close();
+    reverse(text);
+    cout << text << '\n';
 }
